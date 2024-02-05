7a97032e392e817d4488de36793bad2226d7cfb7
kneelian <3547240@gmail.com>
2024-02-05 05:07:25 +0100

    adding simple doubly linked implementation

--- linked-lists/main.cpp
+++ linked-lists/main.cpp
@@ -15,7 +15,26 @@ int main()
 	{
 		printf("%d ", temp.data);
 		temp = *temp.next;
-	}
+	} printf("\n");
+
+	d_list e; e.set(15);
+	d_list f; f.set(20); e.add_child(&f); f.add_parent(&e);
+	d_list g; g.set(30); f.add_child(&g); g.add_parent(&f);
+	d_list h; h.set(50); g.add_child(&h); h.add_parent(&g);
+	h.add_child(&e); e.add_parent(&h);
+
+	d_list tempb = e;
+
+	for (int i = 0; i < 20; ++i)
+	{
+		printf("%d ", tempb.data);
+		tempb = *tempb.next;
+	} printf("and then: ");
+	for (int i = 0; i < 20; ++i)
+	{
+		printf("%d ", tempb.data);
+		tempb = *tempb.prev;
+	} printf("\n");
 
 	return 0;
 }
--- linked-lists/types.hpp
+++ linked-lists/types.hpp
@@ -1,4 +1,3 @@
-
 struct s_list	// singly linked
 {
 	int data = 0;
@@ -28,4 +27,47 @@ struct s_list	// singly linked
 		return (data == 0);
 	}
 		
+};
+
+struct d_list // doubly linked
+{
+	int data = 0;
+	d_list* next = nullptr;
+	d_list* prev = nullptr;
+
+	bool add_child(d_list* arg)
+	{
+		next = arg;
+		return (next != nullptr);
+	}
+
+	bool remove_child()
+	{
+		next = nullptr;
+		return (next == nullptr);
+	}
+
+	bool add_parent(d_list* arg)
+	{
+		prev = arg;
+		return (prev != nullptr);
+	}
+
+	bool remove_parent()
+	{
+		prev = nullptr;
+		return (prev == nullptr);
+	}
+
+	bool set(int arg)
+	{
+		data = arg;
+		return (data == arg);
+	}
+
+	bool clear()
+	{
+		data = 0;
+		return (data == 0);
+	}
 };
