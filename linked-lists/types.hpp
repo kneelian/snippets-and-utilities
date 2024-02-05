fc4e9faa5140b6cc902954fce2f42c76c2193d6e
kneelian <3547240@gmail.com>
2024-02-05 05:31:00 +0100

    adding a simple node type and related functions as well as upping the minimum C++ version to cpp20 for this snippet (requiring `-std=c++20` on my end

--- linked-lists/types.hpp
+++ linked-lists/types.hpp
@@ -1,3 +1,6 @@
+#include <vector>
+#include <unordered_set>
+
 struct s_list	// singly linked
 {
 	int data = 0;
@@ -70,4 +73,55 @@ struct d_list // doubly linked
 		data = 0;
 		return (data == 0);
 	}
+};
+
+struct unode // graph node with unweighted edges
+{
+	int data = 0;
+	//std::vector<unode*> children
+	std::unordered_set<unode*> children;
+	std::unordered_set<unode*> parents;
+
+	bool set(int arg) { data = arg; return (data == arg); }
+	bool clear()      { data = 0;   return (data == 0);   }
+
+	bool add_child(unode* arg) 
+	{ 
+		children.insert(arg);
+		arg->parents.insert(this);
+
+		return (children.contains(arg)); 
+	}
+	bool remove_child(unode* arg)
+	{ 
+		children.erase(children.find(arg));
+		arg->parents.erase(arg->parents.find(this));
+		return !(children.contains(arg));
+	}
+
+	bool remove_parent(unode* arg)
+	{
+		arg->remove_child(this);
+	}
+
+	bool remove_all_children()
+	{
+		for(auto& i : children)
+		{
+			remove_child(i);	
+		}
+		children.clear();
+		return (children.size() == 0);
+	}
+
+	bool remove_all_parents()
+	{
+		for(auto& i : parents)
+		{
+			remove_parent(i);
+		}
+		parents.clear();
+		return (parents.size() == 0);
+	}
+
 };
